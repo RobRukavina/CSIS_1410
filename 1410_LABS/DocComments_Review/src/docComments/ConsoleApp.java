b9b6b4f47e0e5e8fd707a74dc1ce26b2a52df79c
RobRukavina <rehabrob440@gmail.com>
2024-09-04 12:51:35 -0600

    adding stuff from class

---
+++ 1410_LABS/DocComments_Review/src/docComments/ConsoleApp.java
@@ -0,0 +1,22 @@
+package docComments;
+
+import java.util.*;
+
+public class ConsoleApp {
+
+	public static void main(String[] args) {
+		ArrayList<Console> consoleList = new ArrayList<Console>();
+		Console a = new Console("Microsoft", "Xbox", 399.00);
+		Console b = new Console("Microsoft", "Xbox", 399.00);
+		Console c = new Console("Microsoft", "Xbox", 399.00);
+		Console d = new Console("Microsoft", "Xbox", 399.00);
+		consoleList.add(a);
+		consoleList.add(b);
+		consoleList.add(c);
+		consoleList.add(d);
+		
+//		for(int i=0; i > consoleList; i++) {
+//		}
+	}
+
+}
